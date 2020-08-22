017c38bb357e86d2ad2f3b9ba9b075baba7605b0
Documenter.jl <documenter@juliadocs.github.io>
2020-08-22 21:39:21 +0000

    build based on 2953be9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PharmaceuticalClassification","category":"page"},{"location":"#PharmaceuticalClassification","page":"Home","title":"PharmaceuticalClassification","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PharmaceuticalClassification]","category":"page"}]
+}
